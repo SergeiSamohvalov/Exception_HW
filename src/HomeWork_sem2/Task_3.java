5680311da2b480e35adf124f6ef2c8671422e272
sergei sam <ssi1970@mail.ru>
2022-12-13 10:06:08 +0700

    added  src

--- src/HomeWork_sem2/Task_3.java
+++ src/HomeWork_sem2/Task_3.java
@@ -21,17 +21,24 @@ package HomeWork_sem2;
 // * }
 public class Task_3 {
     public static void main(String[] args) {
-        int a = 90;
-        int b = 3;
-        System.out.println(a / b);
+        try {
+            int a = 90;
+            int b = 3;
+            System.out.println(a / b);
+            printSum(23, 234);
+            int[] abc = { 1, 2};
+            abc[3] = 9;
+        }
+        catch (ArithmeticException ex) {
+            System.out.println("Указатель не может указывать на null!");
+        } catch (IndexOutOfBoundsException ex) {
+            System.out.println("Массив выходит за пределы своего размера!");
+        } catch (Throwable ex) {
+            System.out.println("Что-то пошло не так...");
+        }
 
-        printSum(23, 234);
-
-        int[] abc = {1, 2};
-        abc[1] = 9;
     }
-
-    public static void printSum(int a, int b) {
+    public static void printSum(Integer a, Integer b){
         System.out.println(a + b);
     }
 }
